1a64bac0e43be7ebcbbcf49c0a73ef5cc302fd54
Marta Kozak <51427243+iammarta@users.noreply.github.com>
2020-02-03 21:21:18 +0200

    Update js.js

--- js.js
+++ js.js
@@ -925,3 +925,59 @@ for(let i = 1; i<arr.length; i++){
 console.log(arr[i-1]+arr[i]+ arr[i+1]);
 }
 ---------------------------------------------------------------
+Числа Фибоначчи
+
+let one = 0;
+let two = 1;
+
+for(let i = 1; i<=10;i++ ){
+let current = one + two;
+
+one = two;
+two = current;
+console.log(current);
+}
+
+---------------------------------------------------------------
+Выведите на экран следующую пирамидку:
+
+xx
+xxxx
+xxxxxx
+xxxxxxxx
+xxxxxxxxxx
+
+let str = '';
+
+for(let i =0;i<5; i++){
+str +='xx';
+console.log(str+ '<br>');
+}
+---------------------------------------------------------------
+С помощью цикла сформируйте следующий массив:
+
+['xx', 'xxxx', 'xxxxxx', 'xxxxxxxx', 'xxxxxxxxxx']
+
+let str = '';
+let arr = [];
+for(let i =0; i<5;i++){
+str+='xx';
+arr.push(str);
+}
+
+console.log(arr);
+---------------------------------------------------------------
+С помощью цикла сформируйте следующий массив:
+
+['11111', '22222', '33333', '44444', '55555']
+
+
+let arr = [];
+for(let i = 1; i<=5;i++){
+	let str = '';
+for(let j=1;j<=5;j++){
+str +=i;
+}
+	arr.push(str);
+}
+console.log(arr);
